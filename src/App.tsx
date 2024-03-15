32007d1fa1af22daf7bc120ae752e77feb245078
Ervin Torra <ervintorra@alienattorney.com>
2024-03-15 23:16:28 +0100

    carousel and Heros

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import './App.css';
 import { Navbar } from './layouts/NavbarAndFooter/Navbar';
+import { ExploreTop } from './layouts/HomePage/ExploreTop';
+import { Carousel } from './layouts/HomePage/Carousel';
+import { Heros } from './layouts/HomePage/Heros';
 
 function App() {
   return (
-    <Navbar />
+    <div>
+      <Navbar />
+      <ExploreTop/>
+      <Carousel/>
+      <Heros/>
+    </div>
   );
 }
 
--- src/layouts/NavbarAndFooter/Navbar.tsx
+++ src/layouts/NavbarAndFooter/Navbar.tsx
@@ -1,17 +1,17 @@
 
 export const Navbar = () => {
-    return(
-        <nav className='navbar navbar-expand-lg navbar-dark main-color py-3'>
-       <div className='container-fluid'>
+  return (
+    <nav className='navbar navbar-expand-lg navbar-dark main-color py-3'>
+      <div className='container-fluid'>
         <span className='navbar-brand'>Book Store</span>
         <button className='navbar-toggler' type='button'
-         data-bs-toggle = 'collapse' data-bs-target='#navbarNavDropdown'
-         aria-controls='navbarNavDropdown' aria-expanded='false'
-         aria-label='Toggle Navigation' >
+          data-bs-toggle='collapse' data-bs-target='#navbarNavDropdown'
+          aria-controls='navbarNavDropdown' aria-expanded='false'
+          aria-label='Toggle Navigation' >
 
           <span className='navbar-toggler-icon'></span>
-         </button>
-         <div className='collapse navbar-collapse' id='navbarNavDropdown'>
+        </button>
+        <div className='collapse navbar-collapse' id='navbarNavDropdown'>
           <ul className='navbar-nav'>
             <li className='nav-item'>
               <a className='nav-link' href='#'>Home</a>
@@ -28,10 +28,10 @@ export const Navbar = () => {
             </li>
           </ul>
 
-         </div>
+        </div>
 
-       </div>
+      </div>
 
     </nav>
-    );
-    }
+  );
+}
