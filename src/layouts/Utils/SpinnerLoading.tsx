936f01a64f9b2f141e8dc8c2a86743a27a1a0192
Ervin Torra <ervintorra@alienattorney.com>
2024-05-17 21:06:56 +0200

    Why:
    *
    
    This change addresses the need by:
    
    *

--- src/layouts/Utils/SpinnerLoading.tsx
+++ src/layouts/Utils/SpinnerLoading.tsx
@@ -2,11 +2,21 @@
 
 export const SpinnerLoading = () => {
     return (
-        <div className="container m-5 d-flex justify-content-center"
-                style={{height: 550}}>
-            <div className="spinner-border text-primary" role="status">
-                <span className="visually-hidden">Loading...</span>
-            </div>
+        <div className="container m-5 d-flex justify-content-center">
+
+        <div className="spinner">
+        <div></div>   
+        <div></div>    
+        <div></div>    
+        <div></div>    
+        <div></div>    
+        <div></div>    
+        <div></div>    
+        <div></div>    
+        <div></div>    
+        <div></div>    
+      </div>
+        
           
         </div>
     );
